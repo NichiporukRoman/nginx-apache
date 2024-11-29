e1e83a869036a02dbf46f4ddfbdb6e2888f83eed
NichiporukRoman <romannichiporukk@gmail.com>
2024-11-29 11:43:42 +0300

    fixed pseudo compose

--- local/setup-network.sh
+++ local/setup-network.sh
@@ -6,4 +6,4 @@ docker rm nginx
 docker network create --driver=bridge --subnet=192.168.0.0/16 br0
 
 docker run -d -p 8080:8080 --name apache --network br0 nichiporukroman/my-apache-image
-docker run -d -p 88:80 --name nginx -v /etc/ssl/self-signed:/home/ubuntu --network br0 nichiporukroman/my-nginx-image:latest
+docker run -d -p 88:80 --name nginx --network br0 nichiporukroman/my-nginx-image:latest
