7c803c25055ad31e4375903f473ca42014ea3dab
Nichiporuk Roman <89972933+NichiporukRoman@users.noreply.github.com>
2025-01-22 22:34:00 +0300

    Update create-certs.sh

--- local/create-certs.sh
+++ local/create-certs.sh
@@ -3,4 +3,4 @@ cd /etc/ssl/self-signed
 openssl genrsa -out selfsigned.key 2048 
 openssl req -x509 -new -nodes -key selfsigned.key -sha256 -days 365 \
     -out selfsigned.crt \
-    -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=romand.zapto.com/"
+    -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=n8ire.zapto.org/"
